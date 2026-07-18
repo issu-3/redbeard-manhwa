5409c5c377c55cbfa952cf9be9cd5af582e15487
issu-3 <irshadansaripagla@gmail.com>
2026-07-18 22:11:50 +0530

    chore: migrate Prisma to fully support v7 configuration

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig, env } from '@prisma/config'
 export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
-    url: process.env.DIRECT_DATABASE_URL || process.env.POSTGRES_URL || process.env.DATABASE_URL || process.env.PRISMA_DATABASE_URL || '',
+    url: process.env.DIRECT_DATABASE_URL,
   },
 })
